78d641bff8f70ec0905fb573a7b7bb4b0cf90a6d
morelmiles <morelmiles@gmail.com>
2020-03-24 19:41:07 +0300

    Added more functions to iterator functions

--- Iterator_Functions.js
+++ Iterator_Functions.js
@@ -16,3 +16,24 @@ const words = ["You ", "Only", "Live", "Once"];
 
 const abbreviation = words.map(first).join("");
 console.log(abbreviation);
+
+//Differencce between the filter() and every()
+//The filter() works similarly to every() , but instead of returning true if all the elements in an array
+//satsify a Boolean, the function returns a new array consisting of the elements that satsify the Boolean function
+
+const isEven = num => num % 2 == 0;
+
+const isOdd = num => num % 2 != 0;
+
+const nums = [];
+for (let i = 0; i < 20; ++i) {
+  nums[i] = i + 1;
+}
+
+const evens = nums.filter(isEven);
+console.log("Even nubers");
+console.log(evens);
+
+const odds = nums.filter(isOdd);
+console.log("Odd numbers");
+console.log(odds);
