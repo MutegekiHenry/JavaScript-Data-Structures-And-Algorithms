053c2a0200adeeb94b8857086561c93fc0d9a440
morelmiles <morelmiles@gmail.com>
2020-03-25 12:12:07 +0300

    Fixed the typos in Arrays in objects

--- Arrays_In_Objects.js
+++ Arrays_In_Objects.js
@@ -1,32 +1,32 @@
 //An object that stores the weekly observed high temperature. The object has functions for adding new temperature
 //and computing the average of the temperatures sored in the object
 
-const weeklyTemps = () => {
+function weeklyTemps() {
   this.add = add;
   this.average = average;
   this.dataStore = [];
-};
+}
 
-const addTemp = temp => {
+function add(temp) {
   this.dataStore.push(temp);
-};
+}
 
-const average = () => {
+function average() {
   let total = 0;
   for (let i = 0; i < dataStore.length; ++i) {
     total += this.dataStore[i];
   }
   return total / this.dataStore.length;
-};
+}
 
-let thisWeek = weeklyTemps();
+let thisWeek = new weeklyTemps();
 
-this.week.add(75);
-this.week.add(26);
-this.week.add(76);
-this.week.add(29);
-this.week.add(45);
-this.week.add(25);
-this.week.add(43);
+thisWeek.add(75);
+thisWeek.add(26);
+thisWeek.add(76);
+thisWeek.add(29);
+thisWeek.add(45);
+thisWeek.add(25);
+thisWeek.add(43);
 
-console.log(this.weeklyTemps.average);
+console.log(thisWeek.average);
